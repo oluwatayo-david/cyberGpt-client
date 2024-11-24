44235f837a598bb46a0609631779dbbf8d22ce88
David Oluwatayo <126796642+oluwatayo-david@users.noreply.github.com>
2024-11-24 09:20:32 +0100

    Update Dashboard.jsx

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -78,7 +78,7 @@ const DashboardPage = () => {
           </div>
         </div>
       </div>
-      <div className="md:w-1/2 lg:mt-auto  bg-[#2c2937] rounded-md flex  w-full fixed left-0 right-0 bottom-0">
+      <div className="md:w-1/2 lg:mt-auto  bg-[#2c2937] rounded-md flex  w-full fixed left-0 md:relative right-0 bottom-0">
         <form  className="flex items-center justify-between w-full  h-full gap-5 mb-2 " onSubmit={handleSubmit}>
           <input type="text" name="text" placeholder="Ask me anything..."  className="flex-1 p-3 bg-transparent border-none outline-none text-[#ececec]"/>
           <button className="bg-gray-500 rounded-[50%] border-none cursor-pointer p-2 flex items-center justify-center mr-5">
